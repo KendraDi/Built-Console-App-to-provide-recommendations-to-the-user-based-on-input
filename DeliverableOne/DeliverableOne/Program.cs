2a3564bf1a1237adee30c8e1d111cad246366320
KendraDigit config --global user.email kendra.diamond@yahoo.com <kendra.diamond@yahoo.com>
2018-12-20 12:14:08 -0500

    Update Program.cs

--- DeliverableOne/DeliverableOne/Program.cs
+++ DeliverableOne/DeliverableOne/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Globalization;
+using System.Collections.Generic;
+
+//added namespaces for country currencies, attempt to create a list
 
 namespace DeliverableOne
 {
@@ -7,9 +10,8 @@ namespace DeliverableOne
     {
         public static void Main(string[] args)
         {
-            //type conversion average needs to be double data type
             {
-
+                //input from user proper conversion for money (decimal data type)
                 Console.WriteLine("Please enter the first dollar amount.");
                 decimal dollaramount1 = Convert.ToDecimal(Console.ReadLine());
                 Console.WriteLine("Please enter the second dollar amount.");
@@ -17,36 +19,52 @@ namespace DeliverableOne
                 Console.WriteLine("Please enter the third dollar amount.");
                 decimal dollaramount3 = Convert.ToDecimal(Console.ReadLine());
 
-                //total not displayed 
-                //changed from double to decimal data type 
+            //total but instructions do not require total to be displayed
 
                 decimal total = dollaramount1 + dollaramount2 + dollaramount3;
 
-                Console.WriteLine("The average in US currency is {0:C2}", total / 3);
+                //display the average
+
+                Console.WriteLine("The Average is {0:C2}.", total / 3);
 
                 if (dollaramount1 < dollaramount2 && dollaramount1 < dollaramount3)
                 {
-                    Console.WriteLine("the lowest number is {0}", dollaramount1);
+                    Console.WriteLine("The smallest amount is {0:C2}.", dollaramount1);
                     if (dollaramount2 < dollaramount3)
                     {
-                        Console.WriteLine("The highest number is {0}", dollaramount3);
+                        Console.WriteLine("The largest amount is {0:C2}.", dollaramount3);
+                        Console.WriteLine("The total in the various currencies are:");
+                        Console.WriteLine(total.ToString("C2", CultureInfo.CurrentCulture));
+                        Console.WriteLine(total.ToString("C2", CultureInfo.CreateSpecificCulture("da-DK")));
+                        Console.WriteLine(total.ToString("C", CultureInfo.CreateSpecificCulture("ja-JP")));
+                        Console.WriteLine(total.ToString("C2", CultureInfo.CreateSpecificCulture("th")));
                     }
 
                 }
                 else if (dollaramount2 < dollaramount1 && dollaramount2 < dollaramount3)
                 {
-                    Console.WriteLine("The lowest number is {0}", dollaramount2);
+                    Console.WriteLine("The smallest amount is {0:C2}.", dollaramount2);
                     if (dollaramount1 < dollaramount3)
                     {
-                        Console.WriteLine("The highest number is {0}", dollaramount3);
+                        Console.WriteLine("The largest number is {0:C2}.", dollaramount3);
+                        Console.WriteLine("The total in the various currencies are:");
+                        Console.WriteLine(total.ToString("C2", CultureInfo.CurrentCulture));
+                        Console.WriteLine(total.ToString("C2", CultureInfo.CreateSpecificCulture("da-DK")));
+                        Console.WriteLine(total.ToString("C", CultureInfo.CreateSpecificCulture("ja-JP")));
+                        Console.WriteLine(total.ToString("C2", CultureInfo.CreateSpecificCulture("th")));
 
                     }
                     else if (dollaramount3 < dollaramount1 && dollaramount3 < dollaramount2)
                     {
-                        Console.WriteLine("The lowest number is {0}", dollaramount3);
+                        Console.WriteLine("The smallest amount is {0:C2}.", dollaramount3);
                         if (dollaramount2 < dollaramount3)
                         {
-                            Console.WriteLine("The highest number is {0}", dollaramount1);
+                            Console.WriteLine("The largest amount is {0:C2}.", dollaramount1);
+                            Console.WriteLine("The total in the various currencies are:");
+                            Console.WriteLine(total.ToString("C2", CultureInfo.CurrentCulture));
+                            Console.WriteLine(total.ToString("C2", CultureInfo.CreateSpecificCulture("da-DK")));
+                            Console.WriteLine(total.ToString("C", CultureInfo.CreateSpecificCulture("ja-JP")));
+                            Console.WriteLine(total.ToString("C2", CultureInfo.CreateSpecificCulture("th")));
 
                         }
                         else
@@ -55,15 +73,9 @@ namespace DeliverableOne
 
                         }
 
-                        //Show output in requested formats
 
-                        Console.WriteLine("The total in the various currencies are:");
-                        Console.WriteLine(total.ToString("C3", CultureInfo.CurrentCulture));
-                        Console.WriteLine(total.ToString("C3", CultureInfo.CreateSpecificCulture("da-DK"));
-                        Console.WriteLine(total.ToString("C3", CultureInfo.CreateSpecificCulture("ja-JP"));
-                        Console.WriteLine(total.ToString("C3", CultureInfo.CreateSpecificCulture("th-TH"));
-
-
+                        //could not find the proper currency function for Thailand
+                        //tested cases but still issues - need to work on how to use list function and arrays
                     }
                 }
             }
